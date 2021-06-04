8e2fc9409a0cec5ed44dcc780a75cef97e5324e9
Yukun <y.liu@12-5.de>
2021-06-04 15:33:12 +0200

    googlemap

---
+++ whr/inhalt/googlemap.php
@@ -0,0 +1,1 @@
+<iframe class='googlemap' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2569.2347043886302!2d7.011710715712044!3d49.9131695794049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47be2f177022dc43%3A0x84c29c3eef2d460!2sWeinromantikhotel%20Richtershof!5e0!3m2!1szh-CN!2sde!4v1622812834755!5m2!1sde!2sde"  style="border:0;" allowfullscreen="" loading="lazy"></iframe>
