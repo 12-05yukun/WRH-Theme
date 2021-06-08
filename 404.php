da31e4ca485d81b382c28cd670ceb14e2be4f939
Yukun <y.liu@12-5.de>
2021-06-08 17:01:30 +0200

    anpassung

--- 404.php
+++ 404.php
@@ -1,24 +1,6 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
+	<div class='notfound'>Page not found</div>
 
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php esc_html_e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
