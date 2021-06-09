e6716bf36773e1d06e04ae24bc2192cb717caa08
Yukun <y.liu@12-5.de>
2021-06-09 14:19:08 +0200

    fulltext link

--- whr/inhalt/fulltext.php
+++ whr/inhalt/fulltext.php
@@ -2,6 +2,6 @@
   <h1><?php echo $block['headline']?></h1>
   <p><?php echo $block['text']?></p>
   <?php if($block['link']){?>
-  <a href='<?php echo $block['link']['url']?>'><?php echo $block['link']['title']?></a>
+  <a class='link' href='<?php echo $block['link']['url']?>'><?php echo $block['link']['title']?></a>
   <?php }?>
 </div>
