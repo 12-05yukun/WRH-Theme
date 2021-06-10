f50ec8ccf39031518bf0fd7ef1adeceaf5ab9bd4
Yukun <y.liu@12-5.de>
2021-06-10 11:00:58 +0200

    font anpassung

--- whr/whr-function.php
+++ whr/whr-function.php
@@ -7,7 +7,8 @@ require_once('whr-post.php');
 
 function enque_whr_stuff() {
     wp_enqueue_style( 'whr-style', get_template_directory_uri().'/whr/whr.css' );
-    wp_enqueue_style( 'whr-font', 'https://fonts.googleapis.com/css2?family=Noto+Serif&family=Work+Sans:wght@400;700&display=swap"');
+    wp_enqueue_style( 'whr-font-noto', 'https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap');
+    wp_enqueue_style( 'whr-font-work', 'https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap');
     wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
     wp_enqueue_style( 'whr-slider', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
     wp_enqueue_script( 'whr-slider-script', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), '1.8.1', true );
