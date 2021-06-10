82711620303e95709bf1f0ed6fbd578847612bf0
Yukun <y.liu@12-5.de>
2021-06-10 11:09:52 +0200

    angebot

--- whr/inhalt/angebot_list.php
+++ whr/inhalt/angebot_list.php
@@ -26,7 +26,7 @@ foreach($angebots as $an){
   </div>
   <div class='angebot-list-items'>
     <?php if ($angebots){ foreach($angebots as $angebot){ $punkte=get_field('punkte',$angebot);?>
-      <a class='all item visible <?php foreach(get_field('kategorie',$angebot) as $kat){echo ' '.$kat;}?>' href='<?php echo get_field('link',$angebot)['url']?>'>
+      <a class='all item visible <?php foreach(get_field('kategorie',$angebot) as $kat){echo ' '.$kat;}?>' href='<?php echo the_permalink($room->ID)?>'>
         <div class='item-image' >
            <img src='<?php echo get_field('image',$angebot)?>' />
         </div>
