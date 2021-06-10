1c13649db886658833fd0714c37800178fbca349
Yukun <y.liu@12-5.de>
2021-06-10 15:15:26 +0200

    responsive

--- whr/inhalt/angebot_list.php
+++ whr/inhalt/angebot_list.php
@@ -25,8 +25,8 @@ foreach($angebots as $an){
      </div>
   </div>
   <div class='angebot-list-items'>
-    <?php if ($angebots){ foreach($angebots as $angebot){ $punkte=get_field('punkte',$angebot);?>
-      <a class='all item visible <?php foreach(get_field('kategorie',$angebot) as $kat){echo ' '.$kat;}?>' href='<?php echo the_permalink($angebot->ID)?>'>
+    <?php if ($angebots){ foreach($angebots as $angebot){ $punkte=get_field('punkte',$angebot);  $permalink = get_permalink( $angebot->ID );?>
+      <a class='all item visible <?php foreach(get_field('kategorie',$angebot) as $kat){echo ' '.$kat;}?>' href='<?php echo esc_url( $permalink ); ?>'>
         <div class='item-image' >
            <img src='<?php echo get_field('image',$angebot)?>' />
         </div>
