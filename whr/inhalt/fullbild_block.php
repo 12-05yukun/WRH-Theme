128d3c448f92360c18fdb8b661656b97ff08a847
Magnus Westhofen <magnuswesthofen@icloud.com>
2021-07-12 14:14:51 +0200

    block image

--- whr/inhalt/fullbild_block.php
+++ whr/inhalt/fullbild_block.php
@@ -1,19 +1,8 @@
 <div style='background-image:url(<?php echo $block['image']?>)' class='block full-image-block <?php echo $block['image_aligement']?>'>
-  <?php if($block['image_aligement']=='right'){?>
+
     <div class='block-content'>
-      <?php echo $block['text']?>
-      <?php if($block['link']){?>
+        <h2><?php echo $block['headline'];?></h2>
          <a class='link' href='<?php echo $block['link']['url']?>'><?php echo $block['link']['title']?></a>
-      <?php }?>
     </div>
-    <div class='block-image'></div>
-  <?php }else{?>
-    <div class='block-image'></div>
-    <div class='block-content'>
-      <?php echo $block['text']?>
-      <?php if($block['link']){?>
-         <a class='link' href='<?php echo $block['link']['url']?>'><?php echo $block['link']['title']?></a>
-      <?php }?>
-    </div>
-  <?php } ?>
+ 
 </div>
