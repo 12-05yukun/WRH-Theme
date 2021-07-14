1a2767b18664b86736260af2ea7d2adeb506b390
Magnus Westhofen <magnuswesthofen@icloud.com>
2021-07-14 17:19:39 +0200

    favicon & touch

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
 		<title><?php bloginfo( 'name' ); ?> - <?php the_field('title');?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/favicon.ico" rel="shortcut icon">
-		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/favicon.ico" rel="shortcut icon">
+		<link href="/wp-content/uploads/2021/05/richtershof-small.png" rel="apple-touch-icon-precomposed">
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
