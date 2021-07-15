e60ff3b5d463aeec3ff61e2990f49efcc6116443
Magnus Westhofen <magnuswesthofen@icloud.com>
2021-07-15 17:54:00 +0200

    test

--- whr/inhalt/angebot_list.php
+++ whr/inhalt/angebot_list.php
@@ -25,7 +25,7 @@ foreach($angebots as $an){
      </div>
   </div>
   <div class='angebot-list-items'>
-    <?php if ($angebots){ foreach($angebots as $angebot){ $punkte=get_field('punkte',$angebot);  $permalink = get_field('link', $angebot)  ? get_field('link', $angebot):  get_permalink( $angebot->ID );?>
+    <?php if ($angebots){ foreach($angebots as $angebot){ $punkte=get_field('punkte',$angebot);  $permalink = get_field('link', $angebot)  ? get_field('link', $angebot)['link']: get_permalink( $angebot->ID );?>
       <a class='all item visible <?php foreach(get_field('kategorie',$angebot) as $kat){echo ' '.$kat;}?>' href='<?php echo esc_url( $permalink ); ?>'>
         <div class='item-image' >
            <img src='<?php echo get_field('image',$angebot)?>' />
