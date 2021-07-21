b83a45181deed18e36df8c054314f83bff2385ef
Magnus Westhofen <magnuswesthofen@icloud.com>
2021-07-21 13:00:04 +0200

    maps

--- whr/inhalt/googlemap.php
+++ whr/inhalt/googlemap.php
@@ -1,7 +1,7 @@
 
 
-<div class='block left'>
-<iframe class='googlemap' src="<?php echo $block['google_api']?>"  style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+<div class='block map left'>
+<iframe style="width:100%;height:100%;min-height:3vh;" class='googlemap' src="<?php echo $block['google_api']?>"  style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     <div class='block-content'>
       <?php echo $block['content']?>
      
