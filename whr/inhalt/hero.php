4627224aa1eda69c9ed190c7c49d016c206037b4
aleykeyseven <ali_kemal_seven@live.de>
2021-12-10 16:10:23 +0100

    Partner Hotels Block und '1 Booking 1 Tree Logo' im Header eingebaut

--- whr/inhalt/hero.php
+++ whr/inhalt/hero.php
@@ -25,6 +25,11 @@
              </a>
          <?php } ?>
         </div>
+        <a style="overflow:hidden;display:block;width:145px;z-index:3;border:0px;right:0px;position:absolute;bottom: -170px;right: 1%;" href="/bewertungen">
+            <img style="margin-bottom:25px;width: 100%;" src="https://www.theaterhotel-wien.at/wp-content/themes/theaterhotel/img/Libertas_Baumsiegel.png">
+
+
+    </a>
     <?php }?>
 </div>
 <style>
